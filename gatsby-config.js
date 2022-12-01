9f56760261df716bf891c2efbecb4e8a02b98821
sprinter10111 <59802922+sprinter10111@users.noreply.github.com>
2022-12-01 17:55:30 +0100

    na les

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Belgische-Abdijbieren`,
+    description: `lang lopende taak voor cms development`,
+    author: `Siemen De Beukelaer`,
+    siteUrl: `localhost:8000`,
   },
   plugins: [
     `gatsby-plugin-image`,
@@ -30,18 +30,18 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-  ],
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        url:
+          process.env.WPGRAPHQL_URL ||
+          `http://abdijbier.local/graphql`,
+      },
+    }
+    
+  ]
 
   
-    plugins: [
-      {
-        resolve: "gatsby-source-wordpress",
-        options: {
-          url:
-            process.env.WPGRAPHQL_URL ||
-            `http://abdijbier.local/graphql`,
-        },
-      },
-    ],
+    
   
 }
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+// Stap 1: Importeer React
+import * as React from 'react'
+
+// Step 2: Definieer je component
+const AboutPage = () => {
+  return (
+    <main>
+      <title>About Us</title>
+      <h1>About Us</h1>
+      <p>Artist Agency was founded in 1977 by founder, John Doe. AA continues to be at the forefront of art by establishing the careers of our talents on a holistic level -- and setting trends within the industry. </p>
+    </main>
+  )
+}
+
+// Stap 3: Exporteer je component
+export default AboutPage
